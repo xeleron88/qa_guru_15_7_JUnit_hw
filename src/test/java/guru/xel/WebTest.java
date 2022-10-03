ffdd1bc2865c9a8c325c969535b628a0fc03893e
Evgeniy <xelseo@gmail.com>
2022-10-03 17:49:58 +0300

    fix enum

--- src/test/java/guru/xel/WebTest.java
+++ src/test/java/guru/xel/WebTest.java
@@ -15,6 +15,7 @@ import java.util.stream.Stream;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
+import static guru.xel.data.Brands.*;
 
 public class WebTest {
 
@@ -47,18 +48,18 @@ public class WebTest {
         $("#description-wrapper").shouldHave(text(expectedText));
     }
 
-    static Stream<Arguments> checkingForBrandsInMaxfishing() {
+    static Stream<Arguments> checkingForBrandsInMaxFishing() {
         return Stream.of(
-                Arguments.of(List.of("13 Fishing", "Abu Garcia", "Aiko", "Aquatic", "BFT"), Brands.Удилища),
-                Arguments.of(List.of("Abu Garcia", "Adusta", "Aiko", "Anglers System", "Bait Breath"), Brands.Приманки)
+                Arguments.of(List.of("13 Fishing", "Abu Garcia", "Aiko", "Aquatic", "BFT"), UDILISCHA),
+                Arguments.of(List.of("Abu Garcia", "Adusta", "Aiko", "Anglers System", "Bait Breath"), PRIMANKI)
         );
 
     }
-    @MethodSource("checkingForBrandsInMaxfishing")
+    @MethodSource("checkingForBrandsInMaxFishing")
     @ParameterizedTest(name = "Checking first 5 brands {0} in a category {1}")
     void selenideSiteButtonsText(List<String> brands, Brands locale) {
         open("https://maxfishing.ru/catalog/");
-        $$("a").find(text(locale.name())).click();
+        $$("a").find(text(locale.getDesc())).click();
         $$(".custom-control").first(5).shouldHave(CollectionCondition.texts(brands));
 
     }
--- src/test/java/guru/xel/data/Brands.java
+++ src/test/java/guru/xel/data/Brands.java
@@ -1,5 +1,14 @@
 package guru.xel.data;
 
 public enum Brands {
-    Удилища,Приманки
+    UDILISCHA("Удилища"),
+    PRIMANKI("Приманки");
+    private final String desc;
+    Brands(String desc) {
+        this.desc = desc;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
 }
